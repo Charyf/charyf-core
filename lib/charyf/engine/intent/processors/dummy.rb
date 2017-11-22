d2c52ced016681cff66f894cbd8cb45f4a081336
Richard Ludvigh <richard.ludvigh@gmail.com>
2017-11-22 21:38:18 +0100

    Remove test code from dummy intent processor

--- lib/charyf/engine/intent/processors/dummy.rb
+++ lib/charyf/engine/intent/processors/dummy.rb
@@ -8,17 +8,6 @@ module Charyf
         class Dummy < Base
 
         def determine(request, skill = nil)
-
-            #TODO remove
-            if request.text =~ /foo/
-              return Charyf::Engine::Intent.new(:Example, :foo_bar, :foo, 100)
-            end
-            if request.text =~ /bar/
-              return Charyf::Engine::Intent.new(:Example, :foo_bar, :bar, 100)
-            end
-            # TODO TILL HERE
-
-
             unknown
           end
 
